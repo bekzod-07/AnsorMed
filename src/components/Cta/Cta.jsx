9a4ccb93e9a0aa4292a172da17fe155c1aa933d7
bekzod-07 <bekzodsindorov0@gmail.com>
2024-08-06 18:06:10 +0500

    Initialize project using Create React App

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -2,7 +2,19 @@ import "./Cta.css"
 
 function Cta(){
     return(
-        <h1 className='container'>Cta</h1>
+        <div className='cta'>
+            <div className='container'>
+                <div className='cta__section'>
+                    <div className='cta__section__text'>
+                        <h2>Qabulga ro’yxatdan o’ting</h2>
+                    </div>
+                    <div className='cta__section__button'>
+                        <button className='cta__section__button1'>Qo’ng’iroq qilish</button>
+                        <button className='cta__section__button2'>Telegramdan yozish</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 import "./Footer.css"
+import Logo from "../Header/logo.svg";
 
 function Footer(){
     return(
-        <h1 className='container'>Footer</h1>
+        <footer className="footer">
+            <div className='container'>
+                <div className="footer__text">
+                    <div className="footer__text__logo">
+                        <img src={Logo} alt="rasm"/>
+                        <h2>AnsorMed</h2>
+                    </div>
+                    <div className="footer__text__list">
+                        <ul>
+                            <li>Xizmatlar</li>
+                            <li>Dorilar</li>
+                            <li>Asal</li>
+                            <li>Kontaktlar</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
